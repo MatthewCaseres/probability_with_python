80b55cae66229c12740b05654d774ebb13c3c40e
Matthew Caseres <matthewcaseres@outlook.com>
2021-09-17 12:26:04 -0500

    built

---
+++ probability_with_python/_build/jupyter_execute/01-sets.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # An Introduction to Set Theory
+# 
+# ## Sets and elements
+# 
+# Sets are collections of objects. In math we wrap the set in curly braces and separate elements with commas, here is a set with three numbers $\{1,2,4\}$. We often name sets with a capital letter like $A=\{1,2,4\}$. 
+# 
+# Things in the set are called **elements** of the set. The symbol $\in$ means "is an element of the set". So $1 \in \{1,2,4\}$. We put a slash through the symbol if it isn't an element of the set, so $3 \not\in \{1,2,4\}$. Let's see how this works in Python.
+
+# In[1]:
+
+
+# Make a set A = {1,2}
+A = {1,2}
+# The strings are formatted using "f-strings"
+print(f"Our set is {A}")
+print(f"1 is in A? {1 in A}")
+print(f"3 is in A? {3 in A}")
+# We can also construct sets from other "iterables" like lists using the set() function
+print(set([1,2]))
+
+
+# ### Set-builder notation
+# 
+# Sometimes we want to specify an infinite set like the even numbers. We might write this as $\{2,4,6,8,...\}$ and most people will understand. **Set-builder notation** writes it as $\{x | x \text{ is   an   even number}\}$. This is pronounced "the set of x where x is an even number".
+# 
+# We could say $\{x|x \text{ is a solution to } x^3 - 5x^2 - 2x + 10 = 0\}$ if we are having trouble finding the solutions of $x^3 - 5x^2 - 2x + 10 = 0$. The solutions are $\{-\sqrt{2},\sqrt{2},5\}$, so 
+# 
+# $$\{-\sqrt{2},\sqrt{2},5\} =\{x|x \text{ is a solution to } x^3 - 5x^2 - 2x + 10 = 0\}$$
+# 
+# But what does it mean for sets to be equal?
+
+# ## Set equality and subsets
+# 
+# ### Equality
+# 
+# Sets are equal when they have the same elements, **order doesn't matter**. 
+# $$\{1,2\} = \{2,1\}$$
+# Also $\{1,1,2\} = \{1,2\}$, but since duplicate elements are redundant we say that **sets don't have duplicate elements**.
+
+# In[2]:
+
+
+print({1,2} == {2,1})
+print({1,1,2} == {1,2})
+
+
+# ### Subsets
+# 
+# $A \subseteq B$ is pronounced "A is a **subset** of B" and it means that everything in A is also in B. For example $\{1,2\} \subseteq \{1,2,3\}$. Sets are considered subsets of themselves so $\{1,2\} \subseteq \{1,2\}$.
+# 
+# $\{1,2,3\} \supseteq \{1,2\}$ is pronounced "$B$ is a **superset** of $A$".
+# 
+# If sets are subsets of eachother, they are equal. More formally, if $A \subseteq B$ and $B \subseteq A$ then $A = B$. Consider why this is true. This technique is commonly used in proofs of set equality.
+
+# In[3]:
+
+
+A = {1,2}
+B = {1,2,3}
+print(f"A is a subset of B? {A.issubset(B)}")
+print(f"B is a subset of A? {B.issubset(A)}")
+print(f"B is a subset of A? {B.issuperset(A)}")
+
+
+# ## Venn Diagrams and Set Operations
+# 
+# Let's define some sets before we talk about how to visualize them with **venn diagrams** and combine them with **set operations**.
+
+# In[4]:
+
+
+S = {1,2,3,4,5,6,7,8,9,10}
+A = {1,3,5,7}
+B = {2,3,4,5}
+
+
+# 
+# ### Venn Diagrams
+# 
+# In a Venn Diagram each set is represented by a circle. The set $S$ is represented by a black box, think of it as the universe containing all things.
+# 
+# ![](./images/01-sets/Venn.png)
+# 
+# ### Union
+# 
+# $A \cup B$ is "$A$ union $B$". The union of two sets is the set of all elements that are in either set (or in both sets).
+# 
+# ![](./images/01-sets/AUB.PNG)
+
+# In[5]:
+
+
+A.union(B)
+
+
+# ### Intersection
+# 
+# $A \cap B$ is "$A$ intersect $B$". Elements in the intersection must belong to both sets $A$ and $B$.
+# 
+# ![](./images/01-sets/AcapB.PNG)
+
+# In[6]:
+
+
+A.intersection(B)
+
+
+# ### Difference
+# 
+# $A-B$ is the difference of $A$ and $B$ and is set of all elements that are in $A$ but not in $B$. It can also be written $A \backslash B$.
+# 
+# ![](./images/01-sets/A-B.PNG)
+
+# In[7]:
+
+
+# The complement of set A
+A.difference(B)
+
+
+# ### Complement
+# $A^C$ is "the complement of $A$". The complement of a set is all elements that are not in the set. This is relative to our "universe" of possible elements $S$.
+# 
+# ![](./images/01-sets/AC.PNG)
+
+# In[8]:
+
+
+# Python doesn't have a "complement" function on sets. You just take the "universal set" S and subtract A from it.
+S.difference(A)
+
+
+# ## Sample Space and Empty Set
+# 
+# The **sample space** $S$ is the set of all possible elements. In probability there are different outcomes to experiments, and the sample space typically represents all possible outcomes. So when flipping coins the sample space is $\{H,T\}$. There are some identities for the sample space.
+# 
+# $$A \cup S = S$$
+# $$A \cap S = A$$
+
+# In[9]:
+
+
+print(f"{S} == {A.union(S)}")
+print(f"{A} == {A.intersection(S)}")
+
+
+# ## De Morgan's Laws
+# 
+# De Morgan's laws are formulas for the complement of a union or intersection of sets.
+# $$(A \cap B)^C = A^C \cup B^C \\
+# (A \cup B)^C = A^C \cap B^C$$
+# 
+# One way of memorizing these formulas is that you bring the complement inside the parentheses to both sets and flip the union or intersection upside down.
+# 
+# Let's see if these laws make any sense. Let $T$ be the set of tennis players and $H$ be the set of hockey players. $(T \cap H)^C$ is the set of people that don't play both tennis and hockey. $T^C \cup H^C$ is people that don't play tennis or they don't play hockey. If I don't play both sports then I either don't play tennis or I don't play hockey, so $(T \cap H)^C \subseteq T^C \cup H^C$. If I don't play tennis or I don't play hockey then it is true that I don't play both sports, so $T^C \cup H^C \subseteq (T \cap H)^C$. This means that the sets are equal, ponder this for some time. A similar argument can be made for the complement of the union. If this is not convincing spend some time with a Venn diagram and see if you can get it to make sense.
+
+# ## 3 or More Sets
+# 
+# ### Associativity
+# You can take the intersection or union of more than two sets. 
+# $$\{1,2\} \cap \{2,3\} \cap \{3,4\} = \emptyset$$ 
+# There are no elements in the intersection of these three sets because no number appears in all three sets, so this is the empty set. 
+# 
+# Intersections are associative, meaning it doesn't matter what order you take them in. This is also true of unions.
+# $$(A \cap B) \cap C= A \cap (B \cap C)$$ 
+# $$(A \cup B) \cup C= A \cup (B \cup C)$$
+# 
+# ### Distributive Property
+# If you mix together unions and intersections in an expression it isn't associative.
+# $$A \cap (B \cup C) \not= (A \cap B) \cup C$$ 
+# But the distributive property is true of intersections and unions. 
+# $$A \cap (B \cup C) = (A \cap B) \cup (A \cap C) \\
+#  A \cup (B \cap C) = (A \cup B) \cap (A \cup C)$$ 
+# It is easier to remember these distributive formulas by comparing them to the way multiplication distributes over addition. $a(b+c)=ab+ac$. Just pretend that the multiplication is an intersection and the addition is a union.
+# 
+# ### Messy Venn Diagrams
+# You can draw a Venn diagram for three sets with three circles. It gets a little complicated. I wouldn't bother drawing a Venn diagram for 4 sets.
+# ![](./images/01-sets/Venn3.PNG)
+# ![](./images/01-sets/Venn4.PNG)
---
+++ probability_with_python/_build/jupyter_execute/02-counting.py
@@ -0,0 +1,253 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Counting
+# 
+# ## Size of a set
+# 
+# The set $A = \{5,6\}$ has two elements. The is notation for this is $n(A) = 2$. $n$ is a function that takes a set and outputs the number of elements in it: 
+# $$n: \text{Sets} \to \text{Whole Numbers}$$
+# 
+# Let the sample space be $S = \{1,2,3,4,5,6\}$. Since $A=\{5,6\}$, $A^C= \{1,2,3,4\}$ and $n(A^C)=4$. There is a formula for the size of the complement.
+# 
+# $$n(A^C) = n(S) - n(A)=6-2=4$$ 
+# 
+# The size of the complement of a set is the size of the sample space minus the size of the set. 
+
+# In[1]:
+
+
+S = {1,2,3,4,5,6}
+A = {5,6}
+# Use len() to take the size of a set
+print(f"The length of S is {len(S)}")
+print(f"len(S-A) = len(S) - len(A)? {len(S.difference(A)) == len(S) - len(A)}")
+# This isn't true in general, only when S is the sample space.
+B = {10, 12}
+print(f"If B is not a subset of S does it still work? {len(S.difference(B)) == len(S) - len(B)}")
+
+
+# ## Principle of Inclusion-Exclusion
+# 
+# We define sets $A$, $B$ and notice that $n(A \cup B) \not= n(A) +  n(B)$
+
+# In[2]:
+
+
+A = {3,4,5}
+B = {4,5,6}
+print(f"{len(A.union(B))} is the size of the union")
+print(f"{len(A)+len(B)} is the len(A) + len(B)")
+print("These are not equal!")
+
+
+# There is a formula for the size of the union of two sets. This is called the **principle of inclusion-exclusion (PIE)**.
+# 
+# $$n(A \cup B) = n(A) + n(B) - n(A \cap B)$$
+# 
+# We subtract $n(A \cap B)$ because the elements of the intersection are counted twice - once in $n(A)$, and once in $n(B)$. See that $A \cap B = \{4,5\}$ gets counted in both $A= \{3,4,5\}$ and $B=\{4,5,6\}$. Using PIE we see that $n(A \cup B) = 3 + 3 - 2 = 4$
+
+# In[3]:
+
+
+len(A.union(B)) == len(A) + len(B) - len(A.intersection(B))
+
+
+# ### Mutually Exclusive Sets
+# 
+# If $A$ and $B$ have no elements in common then $A \cap B = \emptyset$ and the sets are said to be **mutually exclusive**.
+# 
+# $$A \text{ and } B \text{ are mutually exclusive } \iff A \cap B = \emptyset \iff n(A\cap B) = 0$$
+# 
+# Since $n(A\cap B) = 0$ the principle of inclusion-exclusion allows us to add up the sizes of the sets.
+# 
+# $$
+# \begin{align*}
+# n(A \cup B) &= n(A)+n(B)-n(A \cap B) \text{ inclusion-exclusion} \\ 
+# &= n(A)+n(B)-0 \text{ mutually exclusive}\\
+# &= n(A)+n(B)
+# \end{align*}
+# $$
+# 
+# 
+
+# ### 3 or more sets
+# 
+# The principle of inclusion-exclusion for three sets can be determined using our result for two sets.
+# 
+# $$
+# \begin{align*}
+# n(A\cup (B\cup C)) &= n(A) + n(B\cup C) - n(A \cap (B\cup C)) \\
+# &= n(A) + n(B\cup C) - n((A \cap B) \cup (B \cap C))
+# \end{align*}
+# $$
+# 
+# Then we expand $n(B\cup C)$ and $n((A \cap B) \cup (B \cap C))$ using PIE to get the following:
+# 
+# $$
+# n(A) + n(B) + n(C) - n(A \cap B)-n(B \cap C)-n(C \cap A) + n(A \cap B \cap C)
+# $$
+# 
+# Generally, we add the sizes of the individual sets, subtract the sizes of combinations of two sets, add the sizes of combinations of three sets, subtract the size of combinations of four sets, and add the size of combinations of five sets, etc. So this formula works for the union of arbitrary numbers of sets.
+# 
+# 
+
+# ## Multiplication Principle
+# 
+# ### Tree diagrams
+# 
+# * I have a red shirt and a blue shirt.
+# * I have a green hat, a pink hat, and a black hat.
+# 
+# How many different shirt/hat combinations can I wear?
+# 
+# We can make a **tree diagram** to find that there are 6 combinations.
+# 
+# ![](./images/02-counting/Tree.PNG)
+# 
+# ### Cartesian product
+# 
+# The set $\{(a,b)|a \in A \text{ and } b \in B\}$ is the set of ordered pairs of elements from $A$ and $B$. It has a special name, the **cartesian product**. The notation for this is $A \times B$.
+# 
+# $$A \times B = \{(a,b)|a \in A \text{ and } b \in B\}$$
+# 
+# The size of the cartesian product is the product of the sizes of the sets.
+# 
+# $$n(A \times B) = n(A) \cdot n(B)$$
+# 
+# In our previous example with shirts and hats we can think of the set of shirts $S$ and hats $H$, and the set of combinations as the set $S \times H$, so there are $n(S) \times n(H) = 3 \cdot 2 = 6$ combinations.
+
+# ### Many independent choices
+# 
+# If we are making $n$ independent selections with $a_1, a_2, ..., a_n$ options for our selections the total number of choices is the product of the number of choices for each selection.
+# 
+# $$a_1 \times a_2 \times ... \times a_n=\displaystyle\prod_{k=1}^{n} a_k$$
+# 
+# You can think about this as the cartesian product of many sets. 
+# 
+# There are two options for each flip of a coin. The number of possible sequences of $4$ coin flips is $\prod_{k=1}^{4} 2 = 2^4$.
+
+# In[4]:
+
+
+# Find all sequences of coin flips of length n. We use a list comprehension to generate the new sequences.
+def coin_flips(n):
+  sequences = [[]]
+  for i in range(n):
+    sequences = [seq + ['H'] for seq in sequences] + [seq + ['T'] for seq in sequences]
+  return sequences
+
+print(f"There are {len(coin_flips(4))} sequences")
+print(f"The combinations are: \n {coin_flips(4)}")
+
+
+# It is important that our choices are independent. If I have three shirts and I want to wear two shirts at the same time, I can't wear the same shirt twice. So with my red and blue shirts I can have pairs (red, blue), (blue, red) but not (red, red) or (blue, blue). Let's investigate this.
+
+# ## Permutations
+# 
+# Permutations rearrange elements of a set into ordered outcomes. How many ways can we order the elements of the set $A = \{1,2,3\}$?  Let's draw a tree diagram.
+# 
+# ![](./images/02-counting/permute3large.PNG)
+# 
+# There are three options for our first selection, two options for our second selection, and one option for our third selection. The multiplication principle gives us 6 permutations of the set $\{1,2,3\}$. Generally if $n(A) = m$ there are $m \times (m-1) \times ... \times 2 \times 1 = m!$ permutations of the set $A$.
+# 
+# Sometimes permutations are more obvious than others. The possible outcomes in a three person race are obviously a permutation because we are ordering a set. But order isn't always involved. How many ways can I give three candy bars to my three friends? I have $3$ options for the first candy bar, $2$ options for the second, and $1$ option for the third. If I gave away all the candy bars at the same time there is no sense of "ordering" but still this is a permutation.
+# 
+
+# In[5]:
+
+
+import itertools
+
+# We can use the itertools.permutations() function from the Python standard library
+permutationsGenerator = itertools.permutations([1,2,3])
+
+# The return type of itertools.permutations() is a generator. 
+# Python will not store all the values in memory, but instead will generate them one at a time.
+print(f"The return type of itertools.permutations is {type(permutationsGenerator)}")
+
+# You can use next() to get the next permutation.
+print(f"Permutation 1 is {next(permutationsGenerator)}")
+print(f"Permutation 2 is {next(permutationsGenerator)}")
+
+# We can convert the generator to a list and print them.
+permutations = list(permutationsGenerator)
+print(f"The permutations of [1,2,3] are: \n {permutations}")
+
+# We can write our own function too.
+def generatePermutations(nums: list[int]) -> list[list[int]]:
+  if len(nums) == 0:
+    return [[]]
+  else:
+    permutations = []
+    for i in range(len(nums)):
+      permutations += [[nums[i]] + p for p in generatePermutations(nums[:i] + nums[i+1:])]
+    return permutations
+
+
+# ### k-permutations of n elements
+# 
+# In a $10$ person race how many possible outcomes are there for the top $3$?
+# 
+# There are $10$ options for first place, $9$ options for second place, and $8$ options for third place. The answer is $10 \cdot 9 \cdot 8 = 720$. The general formula for the top $k$ racers in an $n$ person race is:
+# 
+# $$n \times (n-1) \times ... \times (n-k+1) = \frac{n!}{(n-k)!}$$
+# 
+# The notation for this is $P(n,k) = \frac{n!}{(n-k)!}$. $P(n,k)$ is pronounced "$n$ permute $k$".
+
+# In[6]:
+
+
+# The second argument is the number of items to choose from.
+permutations = itertools.permutations([1,2,3], 2)
+print(f"2-permutations of 3 items: {list(permutations)}")
+
+
+# ## Combinations
+# 
+# How many ways can I pick $2$ numbers from the set $\{1,2,3\}$? We have 3 options for the first person, 2 options for the second person, so the answer is $6$? **Wrong**, because this assumes that order is important. The order is not important so the pairs $(1,2)$ and $(2,1)$ are the same. This changes our answer to $3$.
+# 
+# ![](./images/02-counting/P(3,2)vsC(3,2).PNG)
+# 
+# The difference between permutations and combinations is that combinations don't have an order. So there are $2$ permutations for every combination in the previous example. If we chose $3$ numbers there are $3! = 6$ permutations and $1$ combination. In general we see that $P(n,k) = C(n,k) \cdot k!$. Solving for $C(n,k)$ we get:
+# 
+# $$C(n,k) = \frac{P(n,k)}{k!} = \frac{n!}{k!(n-k)!}$$
+
+# In[7]:
+
+
+# We use itertools.combinations()
+combinations = itertools.combinations([1,2,3], 2)
+print(f"2-combinations of 3 items: {list(combinations)}")
+
+# We can also write our own function.
+def combine(n, k):
+  if k == 0:
+    return [[]]
+  else:
+    return [p + [i] for i in range(k, n+1) for p in combine(i-1, k-1)]
+
+
+# ## Variations
+# 
+# Problems can involve several of the previous principles.
+# 
+# ### Piggies and markets
+# 
+# There is a set of $9$ distinct pigs. Three go to the market, three go to Arby's, and three go home. How many ways can this happen?
+# 
+# There are $9 \choose 3$ ways to select the piggies for the market. After that selection is made there are $6 \choose 3$ ways to select the piggies for Arby's. The remaining three piggies go home. The answer is then: 
+# 
+# $${9 \choose 3}{6 \choose 3}{3 \choose 3} = \frac{9!}{3!6!}\cdot \frac{6!}{3!3!} \cdot \frac{3!}{3!0!} = \frac{9!}{3!3!3!}$$ 
+# 
+# There is a general principle at work here. If we divide a set into mutually exclusive subsets then the number on the top is the size of the set and the numbers on the bottom are the size of the subsets. Verify that this is true for dividing a set into two sets (hint: it is just the formula for combinations).
+# 
+# We can rephrase the piggies question as "How many ways can you rearrange the letters of 'aaabbbccc'?" To see that these questions are the same let each position of the letter be a piggy and assign the market, Arby's, and home to the letters a, b, and c. Select positions in groups of three to assign the letters to.
+# 
+# ### We love math problems
+# 
+# How many ways could I rank my 10 favorite math problems from a list of 20 and then do 5 of them? There are $P(20,10)$ possible rankings of my top 10 problems. There are $10 \choose 5$ ways to choose from the selected problems. The answer is then:
+# 
+# $$P(20,10)\times{10 \choose 5} = 168,951,528,345,600$$
+# 
+# 
